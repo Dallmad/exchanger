b7e7be2a01f1b8b58b99c5d8222710bb2e5e2269
Dzmitry <maddal.privod@gmail.com>
2022-02-23 11:38:46 +0300

    create tree exchanger

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Body.module.css'
+
+export const Body = () => {
+    return (
+        <div className={s.body}>
+            <h2>Currency converter</h2>
+            <div>
+
+            </div>
+        </div>
+    );
+};
